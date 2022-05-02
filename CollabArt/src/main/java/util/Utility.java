6964c9a0996b2714bf4ce2e87715405a4fa7c6ed
Max Li <maxl@usc.edu>
2022-05-02 01:15:38 -0700

    please work im on my knees

--- CollabArt/src/main/java/util/Utility.java
+++ CollabArt/src/main/java/util/Utility.java
@@ -13,7 +13,7 @@ public class Utility {
 	    URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
 	    String username = dbUri.getUserInfo().split(":")[0];
 	    String password = dbUri.getUserInfo().split(":")[1];
-	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+	    String dbUrl = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
 	    return DriverManager.getConnection(dbUrl, username, password);
 	}
